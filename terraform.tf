ff39980f2784ce28af3a36cdfab119a68007bdba
nduka145 <nduka1455@gmail.com>
2019-08-05 17:35:42 -0400

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  #access_key = "${params.access_key}"
-  #secret_key = "${params.secret_key}"
+  access_key = "${params.access_key}"
+  secret_key = "${params.secret_key}"
 }
 
 # Create a VPC
