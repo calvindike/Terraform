b71db2b7d7928c785e01e98704f93c209b7e32d6
nduka145 <nduka1455@gmail.com>
2020-02-21 08:06:48 -0500

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -58,19 +58,33 @@ resource "aws_security_group" "terra-SG" {
   vpc_id      = "${aws_vpc.ndukaterraform.id}"
 
   ingress {
-    # TLS (change to whatever ports you need)
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    # Please restrict your ingress to only necessary IPs and ports.
-    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
     cidr_blocks     = ["0.0.0.0/0"]
   }
   tags = {
