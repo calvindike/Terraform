32fe70fa44c9420244c0197839cd238c86e5a4ca
nduka145 <nduka1455@gmail.com>
2021-10-16 21:01:23 -0400

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,6 +3,14 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "testingclass45"
+    key    = "testing/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 # Create a VPC
 resource "aws_vpc" "ndukaterraform" {
   cidr_block = "10.0.0.0/16"
