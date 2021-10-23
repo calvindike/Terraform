19369b7bec90e1d06cccf8633ccede9b17614bf4
nduka145 <nduka1455@gmail.com>
2021-10-23 09:57:40 -0400

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  version = "~> 3.0"
+#  version = "~> 3.0"
   region     = "us-east-1"
 #  aws_access_key_id = "${var.aws_access_key_id}"
 #  aws_secret_access_key = "${var.aws_secret_access_key}"
